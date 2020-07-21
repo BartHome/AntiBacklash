35ad201e6e3db6df855d32e3e3c3d90b235faddf
BartHome <bart_vanpeborgh@hotmail.com>
2020-07-21 19:49:42 +0200

    Add project files.

---
+++ AntiBacklash/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace AntiBacklash
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(238, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Drop your .g or .gcode file here.";
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(64, 181);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(670, 218);
+            this.panel1.TabIndex = 1;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter_1);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(381, 97);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(57, 22);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(718, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter amount of Z-backlash to compensate for in tenths of a millimeter, starting " +
+    "at the second layer (1 = 0.1 mm).";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(82, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(523, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "output file will be placed in the same directory with _out appended to the filena" +
+    "me.";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ AntiBacklash/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+namespace AntiBacklash
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            this.Text = "Z-backlashTool v1.0.0";
+            InitializeComponent();
+        }
+
+        private void panel1_DragEnter_1(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                AlterG(file);
+            }
+            DialogResult result = MessageBox.Show("Do you want to exit the application?", "All done", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+        private void AlterG(string file)
+        {
+            string txt = File.ReadAllText(file);
+            Regex expr = new Regex(@"(G0\s.*?\sZ)(.*)");
+            Match match = expr.Match(txt);
+            string str;
+            match = match.NextMatch(); // first match was not needed
+            while (match.Success)
+            {
+                str = match.Groups[2].Value;
+                double oldZ = double.Parse(str, CultureInfo.InvariantCulture);
+                double newZ = oldZ + ((Convert.ToDouble(numericUpDown1.Value)/10));
+                string newVal = newZ.ToString(CultureInfo.InvariantCulture);
+                txt = txt.Replace(match.Groups[0].Value, match.Groups[1].Value + newVal);
+                match = match.NextMatch();
+            }
+            File.WriteAllText(file.Replace(".g", "_out.g"), txt);
+        }
+    }
+}
